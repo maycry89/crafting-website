9e8660ad59eca285c9e541c10d03cfca3ff07867
maycry89 <maycry@gmx.de>
2024-06-16 20:48:37 +0200

    Mouse Shining and Scrollable Container are working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import MainContent from './components/MainContent';
 import Quote from './components/Quote';
-import Menu from './components/Menu';
-import LightEffect from './components/LightEffect';
 
 function App() {
+  const [mousePos, setMousePos] = useState({ x: window.innerWidth / 2, y: window.innerHeight / 2 });
+
+  const handleMouseMove = (event) => {
+    setMousePos({ x: event.clientX, y: event.clientY });
+  };
+
+  const handleMouseLeave = () => {
+    setMousePos({ x: window.innerWidth / 2, y: window.innerHeight / 2 });
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseleave', handleMouseLeave);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseleave', handleMouseLeave);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <LightEffect />
-      <MainContent />
-      <Quote />
-      <Menu />
+    <div
+      className="App"
+      style={{
+        background: `radial-gradient(circle at ${mousePos.x}px ${mousePos.y}px, white, #d3d3d3)`,
+        height: '100vh',
+        width: '100vw',
+        transition: 'background 0.5s ease',
+      }}
+    >
+      <MainContent></MainContent>
+      
     </div>
   );
 }
--- src/components/LightEffect.js
+++ src/components/LightEffect.js
@@ -1,26 +1,23 @@
-// src/components/LightEffect.js
 import React, { useEffect } from 'react';
-import './LightEffect.css';
 
 const LightEffect = () => {
   useEffect(() => {
     const handleMouseMove = (event) => {
-      const light = document.getElementById('light-effect');
-      if (light) {
-        light.style.left = `${event.pageX - light.clientWidth / 2}px`;
-        light.style.top = `${event.pageY - light.clientHeight / 2}px`;
-      }
+      const x = event.clientX / window.innerWidth * 100;
+      const y = event.clientY / window.innerHeight * 100;
+      document.documentElement.style.setProperty('--mouse-x', `${x}%`);
+      document.documentElement.style.setProperty('--mouse-y', `${y}%`);
     };
 
     window.addEventListener('mousemove', handleMouseMove);
-    
+
     // Cleanup event listener on component unmount
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
     };
   }, []);
 
-  return <div id="light-effect" className="light-effect"></div>;
+  return null;
 };
 
 export default LightEffect;
